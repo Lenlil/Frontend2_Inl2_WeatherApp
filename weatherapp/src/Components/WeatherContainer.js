3c4beff2206635a7494137b768b5a5ee209bd3fa
Lenlil <55378996+Lenlil@users.noreply.github.com>
2020-03-26 17:30:03 +0100

    Allt funkar

--- weatherapp/src/Components/WeatherContainer.js
+++ weatherapp/src/Components/WeatherContainer.js
@@ -2,10 +2,8 @@ import React, {Component} from 'react';
 import SearchCity from './SearchCity';
 import DisplayWeather from './DisplayWeather';
 import DisplayDetails from './DisplayDetails';
-import DisplayError from './DisplayError';
 
 const apiKey ="3836d64ecc391e20540496330ccded06";
-const error = false;
 
 export default class WeatherContainer extends React.Component{
 
@@ -27,16 +25,18 @@ export default class WeatherContainer extends React.Component{
  
     search = (searchvalue) => {  
 
-        try {
+       
             const url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchvalue + "&units=metric&appid=" + apiKey;
 
             fetch(url)
             .then(response => response.json())
-            .then(data => {            
-            
+            .then(data => {                       
+
                 this.setState({todayweatherinfo: data})   
                     
-            });            
+            })
+            .catch(error => alert("Någonting gick fel, försök igen."))                  
+        
                
             const urlfivedays = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchvalue + "&units=metric&appid=" + apiKey;
     
@@ -48,22 +48,13 @@ export default class WeatherContainer extends React.Component{
                         
                 this.setState({dayforecastinfo: filteredarray})                               
                          
-            });     
-        }
-        catch {
-            error = true;        
-        }        
-        
+            })  
+            .catch(error => alert("Din sökning finns ej, försök igen."))  
+         
     }
     
 
-    render = () => {
-        if(error)
-        {
-            return(<React.Fragment>
-                <DisplayError />
-            </React.Fragment>)
-        }
+    render = () => {       
 
         return(<React.Fragment>
 
